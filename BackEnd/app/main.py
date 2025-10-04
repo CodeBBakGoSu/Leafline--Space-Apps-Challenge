7c5abb552473957416d24e7aa50778075fbdb41d
theeunseojo <68554926+theeunseojo@users.noreply.github.com>
2025-10-04 20:37:21 +0900

    게시글 관련 기능 추가 (#32)
    게시글 관련 기능 추가 
    main.py 파일 충돌 수정

--- BackEnd/app/main.py
+++ BackEnd/app/main.py
@@ -8,6 +8,7 @@ from app.todo.router import router as todo_router
 from app.dashBord.weather_router import router as weather_router
 from app.data.profile_router import router as profile_router
 from app.chart.chart_router import router as chart_router  # chart_router 임포트 추가
+from app.community.community_router import router as community_router  # community_router 임포트 추가
 
 app = FastAPI(
     title=settings.APP_NAME,
@@ -28,7 +29,11 @@ app.add_middleware(
 )
 
 # 라우터 등록
-
+app.include_router(profile_router, prefix="")
+app.include_router(todo_router, prefix="")
+app.include_router(weather_router, prefix="")
+app.include_router(chart_router, prefix="")  # chart_router 등록
+app.include_router(community_router, prefix="")  # community_router 등록
 app.include_router(profile_router, prefix="/api")
 app.include_router(todo_router, prefix="/api")
 app.include_router(weather_router, prefix="/api")
